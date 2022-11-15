d4c5e13d1ef576708067fb1da6a17b4f5cb26102
nguyenzung <nguyenvietdungcs52@gmail.com>
2022-11-15 11:22:53 +0700

    update instruction by following polish notation

--- vm/instructions.go
+++ vm/instructions.go
@@ -10,17 +10,17 @@ var (
 	PUSH   uint8 = 0x01
 	ADD    uint8 = 0x04
 	SUB    uint8 = 0x05
-	MUL    uint8 = 0x06 // stack[i - 1] = stack[i] - stack[i-1]
+	MUL    uint8 = 0x06 // stack[i - 1] = stack[i - 1] - stack[i]
 	DIV    uint8 = 0x07
 	AND    uint8 = 0x08
 	OR     uint8 = 0x09
 	NAND   uint8 = 0x0A
 	XOR    uint8 = 0x0B
 	NOT    uint8 = 0x0C
-	LT     uint8 = 0x0D // stack[i - 1] < stack[i]
-	GT     uint8 = 0x0E // stack[i - 1] > stack[i]
-	LTE    uint8 = 0x0F // stack[i - 1] <= stack[i]
-	GTE    uint8 = 0x10 // stack[i - 1] >= stack[i]
+	LT     uint8 = 0x0D // stack[i - 1] > stack[i]
+	GT     uint8 = 0x0E // stack[i - 1] < stack[i]
+	LTE    uint8 = 0x0F // stack[i - 1] >= stack[i]
+	GTE    uint8 = 0x10 // stack[i - 1] <= stack[i]
 	EQ     uint8 = 0x11 // stack[i - 1] == stack[i]
 	SHL    uint8 = 0x12 // Shift left
 	SHR    uint8 = 0x13 // Shift left
@@ -29,8 +29,8 @@ var (
 	DEC    uint8 = 0x21
 	DUP    uint8 = 0x38
 	SWAP   uint8 = 0x39
-	LOAD   uint8 = 0x40
-	STORE  uint8 = 0x41
+	LOAD   uint8 = 0x40 // Load from memory that point by stack[i]
+	STORE  uint8 = 0x41 // Store stack[i-1] to memory that point by stack[i]
 	SLOAD  uint8 = 0x60
 	SSTORE uint8 = 0x61
 	CALL   uint8 = 0x80
