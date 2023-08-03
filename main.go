dbd312234c3da059e97026765be7b9c0acffd16c
nguyenzung <nguyenvietdungcs52@gmail.com>
2023-08-03 14:42:28 +0700

    clean main.go

--- main.go
+++ main.go
@@ -8,14 +8,9 @@ import (
 
 func main() {
 	fmt.Println("Virtual Machine")
-
-	var a, b uint64 = 12, 18
-	fmt.Println(a - b)
-	a = 0
-	b = 1
-	fmt.Println(^a, a-b)
-
 	myVM := vm.MakeVM(8 * 10000000)
+
+	// setup bootstrap program is stored on ROM
 	myVM.AddInstruction(vm.MakePUSH(15))  // 0
 	myVM.AddInstruction(vm.MakePUSH(25))  // 1
 	myVM.AddInstruction(vm.MakeADD())     // 2
@@ -45,5 +40,4 @@ func main() {
 	myVM.StartVM()
 	myVM.DebugMemory()
 	myVM.DebugStack()
-
 }
